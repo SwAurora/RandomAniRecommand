d03286ec3a3d7df2e4f15956b8c4cea0c0c7a370
SwAurora <rlatjddnr74@gmail.com>
2023-01-16 23:35:54 +0900

    2023-01-16 master(ver1)

---
+++ src/test/java/com/sw/randomanirecommand/RandomAniRecommandApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sw.randomanirecommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomAniRecommandApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
