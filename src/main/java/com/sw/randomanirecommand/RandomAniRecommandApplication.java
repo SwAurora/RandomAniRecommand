fadccceeade53e0ab77a5293dbab5675ef47c370
SwAurora <rlatjddnr74@gmail.com>
2023-02-13 20:50:30 +0900

    2023-02-13 master(ver1)

--- src/main/java/com/sw/randomanirecommand/RandomAniRecommandApplication.java
+++ src/main/java/com/sw/randomanirecommand/RandomAniRecommandApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RandomAniRecommandApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(RandomAniRecommandApplication.class, args);
     }
-
 }
