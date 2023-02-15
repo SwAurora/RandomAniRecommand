201821d3ec94a2e503b567d169dc2c60fc08d19f
SwAurora <rlatjddnr74@gmail.com>
2023-02-16 08:26:20 +0900

    2023-02-16 master(ver6)

--- src/main/resources/static/js/showByRandom.js
+++ src/main/resources/static/js/showByRandom.js
@@ -8,5 +8,9 @@ function goInfo(title)
 {
     title = title.replace('\`', '%60');
     title = title.replace('\&', '%26');
+    title = title.replace('\-', '%2D');
+    title = title.replace('\=', '%3D');
+    title = title.replace('\[', '%5B');
+    title = title.replace('\]', '%5D');
     location.href = "/anime/info?title=" + title;
 }
